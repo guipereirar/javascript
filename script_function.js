c5b6c2afba7e53979a1b923619ac5782d522fb88
unknown <gpereira383@Gmail.com>
2024-02-21 22:34:41 -0300

    feat: first commit

---
+++ script_function.js
@@ -0,0 +1,8 @@
+var contador = 0;
+function converterTemp(temp) {
+  //   return temp + 1;
+  contador++;
+  document.getElementById("saida").innerText = contador;
+}
+
+// alert(converterTemp(20));
